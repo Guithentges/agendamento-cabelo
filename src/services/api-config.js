c8450c33b8be2874639ec5695d76e6b056d44827
Guithentges <guithentges@gmail.com>
2025-02-26 22:00:15 -0300

    railway

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,3 +1,3 @@
 export const apiConfig = {
-    baseURL : "https://railway.com/project/331de73f-91d4-4129-acb2-4005876de008?environmentId=d806c135-6298-4475-a38c-3092f8130b47"
+    baseURL : "https://agendamento-cabelo-production.up.railway.app/"
 }
