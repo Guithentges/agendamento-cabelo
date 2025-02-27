2569686cfe644702854052804c7734e0c3de8818
Guithentges <guithentges@gmail.com>
2025-02-26 21:42:08 -0300

    deploy no railway

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,3 +1,3 @@
 export const apiConfig = {
-    baseURL : "https://agendamento-cabelo.onrender.com"
+    baseURL : "https://railway.com/project/331de73f-91d4-4129-acb2-4005876de008?environmentId=d806c135-6298-4475-a38c-3092f8130b47"
 }
